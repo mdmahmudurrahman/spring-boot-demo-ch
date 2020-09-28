986d1179fcaf9e89386ff8b724c96cea028c81f5
mdmahmudurrahman <mailtommrr@gmail.com>
2020-09-28 08:09:06 +0200

    Spring boot build in and custom properties

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,11 +2,22 @@ package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamhName;
+	
+	@GetMapping("/teamInfo")
+	public String getTeamInfo() {
+		return "Coach name: "+coachName+" and team name: "+teamhName;
+	}
 	
 	@GetMapping("/")
 	public String sayHello() {
